e8b4a45d2545218868d9f52dbf2568c00e1b6e6d
hungntt <minatosnow@gmail.com>
2021-08-17 22:27:00 +0700

    Remove redundant files and reformat

--- env/simulation.py
+++ env/simulation.py
@@ -62,6 +62,7 @@ class InsulinControl:
     def formula(self):
         """
         All formulas are coded here. Uses later.
+        """
         x1 = self.g_init
         x2 = self.v_init
         x3 = self.i_init
@@ -70,55 +71,50 @@ class InsulinControl:
         t_time = None
         J_shape = (4, 4)
         J_x = np.zeros(J_shape)
-        J_x[0, :] = np.array([-self.P1  -x2, -x1, 0, 0])
+        J_x[0, :] = np.array([-self.P1 - x2, -x1, 0, 0])
         J_x[1, :] = np.array([0, -self.P2, self.P3, 0])
-        J_x[2, :] = np.array([self.gamma*t_time, 0, -self.n,1])
-        J_x[3, :]= np.array([0, 0, 0, -1/self.a])
+        J_x[2, :] = np.array([self.gamma * t_time, 0, -self.n, 1])
+        J_x[3, :] = np.array([0, 0, 0, -1 / self.a])
 
         J_u = np.array([[0],
                         [0],
                         [0],
-                        [1/self.a]])
+                        [1 / self.a]])
         x = None
         u = None
 
-        #First fomula
-        x_dot = J_x*x + J_u*u
-        y = np.array([1,0,0,0])*x
-
+        # First formula
+        x_dot = J_x * x + J_u * u
+        y = np.array([1, 0, 0, 0]) * x
 
         A = J_x
         B = np.array([[0],
-                        [0],
-                        [0],
-                        [0.5]]) # B = J_u where u = 2
-        C = np.array([1,0,0,0])
+                      [0],
+                      [0],
+                      [0.5]])  # B = J_u where u = 2
+        C = np.array([1, 0, 0, 0])
 
-        a1 =None
-        a2= None
+        a1 = None
+        a2 = None
         a3 = None
         a4 = None
 
-        totient_A = pow(A,4) + a1*pow(A,3) + a2*pow(A,2) +a3*A,1+ a4*np.eye(4)
+        totient_A = pow(A, 4) + a1 * pow(A, 3) + a2 * pow(A, 2) + a3 * A, 1 + a4 * np.eye(4)
         matrix_CA = np.array([[C],
-                      [C*A],
-                      [C*(A**2)],
-                      [C*(A**3)]])
+                              [C * A],
+                              [C * (A ** 2)],
+                              [C * (A ** 3)]])
         another_term = np.array([[0],
-                      [0],
-                      [0],
-                      [1]])
-        K_varepsilon = totient_A*np.linalg.inv(matrix_CA)*another_term
+                                 [0],
+                                 [0],
+                                 [1]])
+        K_varepsilon = totient_A * np.linalg.inv(matrix_CA) * another_term
 
-        K = np.array([0,0,0,1])*np.linalg.inv(np.array(B, A*B, A*A*B, (A**3)*B))*totient_A
+        K = np.array([0, 0, 0, 1]) * np.linalg.inv(np.array(B, A * B, A * A * B, (A ** 3) * B)) * totient_A
 
         # x_tilde(t) is used to denote the observed state vector
         # x_tilde is the estimate state
         x_tilde = None
 
-        # Second fomula
-        x_tilde_dot = (A - K_varepsilon*C -B*K)*x_tilde + K_varepsilon*y
-        """
-
-
-
+        # Second formula
+        x_tilde_dot = (A - K_varepsilon * C - B * K) * x_tilde + K_varepsilon * y
